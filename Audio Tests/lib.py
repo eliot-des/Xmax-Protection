3eb2ba5a976fcbbe99b792ffda7f487f13a9cda6
Eliot Deschang <eliot.deschang@gmail.com>
2024-09-28 10:03:27 +0200

    Move functions to lib file.

--- Audio Tests/lib.py
+++ Audio Tests/lib.py
@@ -117,3 +117,82 @@ def multibandChebyshev1Filters(order, fc, fs, rp):
         sos[i] = sig.cheby1(order, rp, (fc[i-1], fc[i]), 'bandpass', fs = fs, output='sos')
         
     return sos
+
+
+
+def dynamic_peak_follower1(x, attack_time, release_time, sample_rate):
+    x_peak = 0.0
+    x_peak_output = np.zeros_like(x)
+
+    # Calculate attack and release coefficients based on provided times
+    attack_coeff  = 1 - np.exp(-2.2 / (attack_time * sample_rate))
+    release_coeff = 1 - np.exp(-2.2 / (release_time * sample_rate))
+
+    for n in range(len(x)):
+        abs_x = np.abs(x[n])
+        
+        delta = abs_x - x_peak
+        
+        if delta > 0:
+            x_peak += attack_coeff * delta
+        else:
+            x_peak += release_coeff * delta
+        
+        # Store the result in the output array
+        x_peak_output[n] = x_peak
+
+    return x_peak_output
+
+def dynamic_peak_follower2(x, attack_time, release_time, Fs):
+    
+    x_peak = 0  
+    x_peak_output = np.zeros_like(x)
+    
+    # Attack and release coefficients based on time constants
+    attack_coeff  = 1 - np.exp(-2.2 / (attack_time * Fs))
+    release_coeff = 1 - np.exp(-2.2 / (release_time * Fs))
+
+    # Process each sample
+    for n in range(len(x)):
+
+        abs_x = np.abs(x[n])
+        
+        if abs_x > x_peak:
+            x_peak = (1 - attack_coeff) * x_peak + attack_coeff * abs_x
+        else:
+            x_peak = (1 - release_coeff) * x_peak
+        
+                
+        # Store the result
+        x_peak_output[n] = x_peak
+    
+    return x_peak_output
+
+#RMS Measurement
+def dynamic_rms_follower(x, averaging_time, Fs):
+    
+    x2_rms = 0  
+    x_rms_output = np.zeros_like(x)
+
+    averaging_coeff = 1 - np.exp(-2.2 / (averaging_time * Fs))
+
+    for n in range(len(x)):
+        #squared RMS value
+        x2_rms = (1 - averaging_coeff) * x2_rms + averaging_coeff * x[n]**2
+        x_rms_output[n] = np.sqrt(x2_rms)
+    
+    return x_rms_output
+
+
+def gain_factor_smoothing(x, attack_coeff, release_coeff):
+    y = np.zeros_like(x)
+
+    for n in range(1, len(x)):
+        if x[n] > x[n-1]:
+            k = attack_coeff
+        else:
+            k = release_coeff
+
+        y[n] = (1 - k) * y[n-1] + k * x[n]
+
+    return y
--- State Space Model/ss_loudspeaker.py
+++ State Space Model/ss_loudspeaker.py
@@ -12,7 +12,7 @@ loudspeakers = {'full-range1' :'Dayton_CE4895-8',
                 'woofer3': 'Dayton_RS150-4',
                 'subwoofer1': 'B&C_15FW76-4'}
 
-loudspeaker = loudspeakers['subwoofer1']
+loudspeaker = loudspeakers['full-range2']
 
 with open(f'Dataset_T&S/{loudspeaker}.txt', 'r') as f:
     lines = f.readlines()
