f580b0d290c4145b31936cc396e67dd5e0023b51
spreadflo <florian_marie@ymail.com>
2024-10-06 20:35:59 +0200

    Pysical filter feedback working.

--- Audio Tests/Feedback_Physical_Approach.py
+++ Audio Tests/Feedback_Physical_Approach.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sig 
 from scipy.io.wavfile import read
-from lib import normalize, dynamic_peak_follower2, gain_factor_smoothing_sbs_bis, dynamic_peak_follower_sbs
+from lib import dynamic_peak_follower2, gain_factor_smoothing_sbs_bis, dynamic_peak_follower_sbs, gain_factor_smoothing_sbs
 import sys
 
 plt.rc('lines', linewidth=2)
@@ -17,14 +17,19 @@ plt.rc('legend', fontsize=14)
 Fs, v = read('Audio Tests/Thriller.wav')
 t = np.arange(0, len(v)/Fs, 1/Fs)
 '''
-#create a sweep/ chirp signal
+#create a sweep/chirp signal
 Fs = 48000
-t = np.arange(0, 1, 1/Fs)
-v = sig.chirp(t, f0=10000, f1=10, t1=1, method='logarithmic')
-#v = normalize(v)    #normalize the signal to 1
+t = np.arange(0, 5, 1/Fs)
+v = sig.chirp(t, f0=10000, f1=10, t1=5, method='logarithmic')
 
 A = 4               #gain of the amplifier -> Max tension in volts
 u = A*v             #tension in volts
+
+tstart   = 0            #start time in seconds
+duration = 5            #duration time in seconds
+
+u = u[int(tstart*Fs):int((tstart+duration)*Fs)]
+t = t[int(tstart*Fs):int((tstart+duration)*Fs)]
 
 # fig, ax = plt.subplots()
 # ax.plot(t, u, label='Sweep')
@@ -36,17 +41,12 @@ u = A*v             #tension in volts
 # sys.exit()
 
 
-tstart   = 0            #start time in seconds
-duration = 1            #duration time in seconds
-
-u = u[int(tstart*Fs):int((tstart+duration)*Fs)]
-t = t[int(tstart*Fs):int((tstart+duration)*Fs)]
-
-
 #================================================================================
 #==== Importing speaker TS parameters and defining the displacement filter ======
 #================================================================================
 
+Xmax = 1   #in mm
+Xmax_= Xmax/1e3 #in m
 # Thiele-small parameters
 loudspeakers = {'full-range1' :'Dayton_CE4895-8',
                 'full-range2' :'Dayton_HARB252-8',
@@ -61,6 +61,12 @@ with open(f'Dataset_T&S/{loudspeaker}.txt', 'r') as f:
     lines = f.readlines()
     for line in lines:
         exec(line)
+
+
+#optimal minimal compliance to not 
+R = (Xmax_*Rec/(A*Bl*Cms))
+print('R = ', R)
+
 
 #analog coefficients
 # b = np.array([0, 0, 0, Bl])
@@ -91,69 +97,21 @@ b = b/a0
 #================================================================================
 #========== Filter the input voltage to get an estimated displacment ============
 #================================================================================
-
-#Displacement calculation without feedback control
-
-# x2 = sig.lfilter(b, a, u)    #displacement in m
-
-x2 = np.zeros_like(u)
-d = np.zeros(4) # memory buffer for Direct Form I
-
-for n in range(len(u)):
-    x2[n] = b[0]*u[n] + b[1]*d[0] + b[2]*d[1] - a[1]*d[2] - a[2]*d[3]
-
-    d[1] = d[0]
-    d[0] = u[n]
-    d[3] = d[2]
-    d[2] = x2[n]
-
-x2*= 1e3                     #displacement in mm
-
-
-# fig, ax = plt.subplots()
-# ax.plot(t, u, label='Sweep')
-# ax.plot(t, x2, label='Estimated displacement')
-# ax.set_xlabel('Time [sec]')
-# ax.set_ylabel('Amplitude [a.u]')
-# ax.legend()
-# ax.grid()
-# plt.show()
-
-# sys.exit()
-
+x2 = np.zeros_like(u)        #displacement in m
+x2 = sig.lfilter(b, a, u)    #displacement in m
+x2 = x2*1000                #displacement in mm
 
 #================================================================================
 #====================== Compensation filter definition ==========================
 #========================= Initializing parameters ==============================
 #================================================================================
 
-#feedback control algorithm
-
-#Initializations
-
-Xmax = 1 #in mm 
-Xmargin = 0.5
-
-x        = np.zeros(len(u))    #displacement in mm
-x_lim    = np.zeros(len(u))    #limited displacement in mm
-x_peak   = np.zeros(len(u))    #enveloppe estimator in mm
 Cms_comp = np.zeros(len(u))    #compensation compliance to be adjusted sample by sample
+Cms_max = Cms             #initial compensation compliance equal (almost) to the real one of the speaker
+Cms_min = 0.9*R*Cms              #minimum compensation compliance is 10% of the real Cms (CHOICE)
+ratio = 0.6
+Cms_comp[0] = Cms_max #ratio*Cms
 
-Cms_max = 0.95*Cms             #initial compensation compliance equal (almost) to the real one of the speaker
-Cms_min = 0.4*Cms              #minimum compensation compliance is 10% of the real Cms (CHOICE)
-Cms_comp[0] = Cms_max
-
-attack_peak    = 0.00005     # Attack time in seconds
-release_peak   = 0.1       # Release time in seconds
-attack_smooth  = 0.002     # Attack time for the gain smoothing function
-release_smooth = 0.005      # Release time for the gain smoothing function
-
-# print("k attack: {} sec".format(round(1 - np.exp(-2.2 / (attack_smooth * Fs)), 3)))
-# print("k release: {} sec".format(round(1 - np.exp(-2.2 / (release_smooth * Fs)), 3)))
-
-# sys.exit()
-
-#create the compensation filter based on the physical approach
 B_comp = A_LF
 A_comp = np.array([Mms, Rms+Bl**2/Rec, 1/Cms_comp[0]])
 
@@ -168,6 +126,11 @@ a_comp[0] = A_LF[0]*4*Fs**2+A_LF[1]*2*Fs+A_comp[2]
 a_comp[1] = -2*A_LF[0]*4*Fs**2+2*A_comp[2]
 a_comp[2] = A_LF[0]*4*Fs**2-A_LF[1]*2*Fs+A_comp[2]
 
+#normalize coeff
+a0_comp = a_comp[0]
+a_comp = a_comp/a0_comp
+b_comp = b_comp/a0_comp
+
 f = np.geomspace(1, Fs/2, 1000)
 w   = 2*np.pi*f
 
@@ -175,41 +138,83 @@ w   = 2*np.pi*f
 # _, H = sig.freqz(b_comp, a_comp, worN=f, fs=Fs)
 
 # fig, ax = plt.subplots()
-
 # ax.semilogx(f, 20*np.log10(np.abs(h)), '--r', label='theoritical')
 # ax.semilogx(f, 20*np.log10(np.abs(H)), '-.b', label='analytically bilinearized')
 # ax.set_xlabel('Frequency  [Hz]')
 # ax.set_ylabel('Gain [dB]')
 # ax.grid(which='both')
-# ax.legend()
+# ax.legend(loc='lower right')
 # plt.show()
+# sys.exit()
 
+x_lim = np.zeros_like(x2)
+d_TDFII = np.zeros(2)   # memory buffer for Transposed Direct Form II
+
+for i in range(len(x2)):
+    x_lim[i] = b_comp[0]*x2[i] + d_TDFII[0]
+
+    d_TDFII[0] = b_comp[1]*x2[i] - a_comp[1]*x_lim[i] + d_TDFII[1]
+    d_TDFII[1] = b_comp[2]*x2[i] - a_comp[2]*x_lim[i]
+
+# fig, ax = plt.subplots()
+# ax.plot(t, u, label='Sweep')
+# ax.plot(t, x2, label='Displacement')
+# ax.plot(t, x_lim, label=f'Filtered displacement - Cms bis ={np.int16(ratio*100)}% Cms0')
+# ax.set_xlabel('Time [sec]')
+# ax.set_ylabel('Amplitude [a.u]')
+# ax.legend(loc='lower left')
+# ax.grid()
+# plt.show()
 # sys.exit()
 
 
 #================================================================================
-#================================== Main loop ===================================
+#================== Main loop - feedback control algorithm ======================
 #================================================================================
+
+#Initializations
+
+wr = 2*np.pi*fs
+
+x        = np.zeros(len(u))    #displacement in mm
+x_lim    = np.zeros(len(u))    #limited displacement in mm
+x_peak   = np.zeros(len(u))    #enveloppe estimator in mm
+
+attack_peak    = 0.00005       # Attack time in seconds
+release_peak   = 0.5         # Release time in seconds
+attack_smooth  = 0.01         # Attack time for the gain smoothing function
+release_smooth = 0.5         # Release time for the gain smoothing function
+# print("k attack: {} sec".format(round(1 - np.exp(-2.2 / (attack_smooth * Fs)), 3)))
+# print("k release: {} sec".format(round(1 - np.exp(-2.2 / (release_smooth * Fs)), 3)))
+
+# sys.exit()
 
 d_DFI = np.zeros(4)     # memory buffer for Direct Form I
 d_TDFII = np.zeros(2)   # memory buffer for Transposed Direct Form I
 a_comp = np.zeros(3)    # denumerator coefficient of the compensation filter in the z domain
+u_hp = np.zeros_like(u)
 
-for i in range(1, len(u)):
+for i in range(1, len(u_hp)):
     
-    #we first estimate the displacement from the u[i] sample
-    x[i] = b[0]*u[i] + b[1]*d_DFI[0] + b[2]*d_DFI[1] - a[1]*d_DFI[2] - a[2]*d_DFI[3]
+    #we first estimate the displacement from the u_hp[i] sample
+    x[i] = b[0]*u_hp[i-1] + b[1]*d_DFI[0] + b[2]*d_DFI[1] - a[1]*d_DFI[2] - a[2]*d_DFI[3]
 
     d_DFI[1] = d_DFI[0]
-    d_DFI[0] = u[i]
+    d_DFI[0] = u_hp[i-1]
     d_DFI[3] = d_DFI[2]
     d_DFI[2] = x[i]
 
-    x_peak[i] = dynamic_peak_follower_sbs(x[i]*1000, x_peak[i-1], attack_peak, release_peak, Fs)
+    x[i] *= 1000
+
+    # x_peak[i] = dynamic_peak_follower_sbs(x[i], x_peak[i-1], attack_peak, release_peak, Fs) # x_peak in mm
     
+    # Klippel trick
+    dx = (x[i] - x[i-1]) * Fs
+    x_peak[i] = np.sqrt(x[i]**2 + (dx/wr)**2)
+
     #we then compare the peak of the displacement signal with Xmax
-    # if np.abs(x[i]*1000) > Xmax:
-    if np.abs(x_peak[i]) > Xmax:
+    if np.abs(x[i]) > Xmax:
+    # if x_peak[i] > Xmax:
         # Cms_comp[i] = Cms_min
         Cms_target = Cms_min
     else:
@@ -236,11 +241,11 @@ for i in range(1, len(u)):
     a_comp = a_comp/a0_comp
     b_comp = b_comp/a0_comp
 
-    # #... then apply it to the tension signal
-    x_lim[i] = b_comp[0]*x[i] + d_TDFII[0]
+    #... then apply it to the tension signal
+    u_hp[i] = b_comp[0]*u[i] + d_TDFII[0]
 
-    d_TDFII[0] = b_comp[1]*x[i] - a_comp[1]*x_lim[i] + d_TDFII[1]
-    d_TDFII[1] = b_comp[2]*x[i] - a_comp[2]*x_lim[i]
+    d_TDFII[0] = b_comp[1]*u[i] - a_comp[1]*u_hp[i] + d_TDFII[1]
+    d_TDFII[1] = b_comp[2]*u[i] - a_comp[2]*u_hp[i]
 
 # sys.exit()
 
@@ -251,9 +256,10 @@ for i in range(1, len(u)):
 
 fig, ax = plt.subplots()
 
-ax.plot(t, x*1000, '-.b', linewidth=1, label='Estimated displacement (feedback)')
-ax.plot(t, x_peak, 'r-', linewidth=1, alpha=0.4, label='x_{peak}')
-ax.plot(t, x_lim*1000, 'm', label='Limited displacement (feedback)')
+ax.plot(t, x, '-.b', linewidth=1, label=f'Estimated displacement (feedback)\n Attack {np.int16(attack_smooth*1000)} ms\n Release {np.int16(release_smooth*1000)} ms')
+# ax.plot(t, x_peak, 'r-', linewidth=1, alpha=0.4, label='x_{peak}')
+ax.plot(t, x2, linewidth=1, alpha=0.2, label='Displacement (no feedback)')
+# ax.plot(t, u_hp, 'm', label='Limited displacement (feedback)')
 ax.axhline(y=Xmax, color='red', linestyle='--', label='+Xmax')
 ax.axhline(y=-Xmax, color='red', linestyle='--', label='-Xmax')
 ax.set_ylabel('Amplitude [mm]')
@@ -262,35 +268,9 @@ ax.legend(loc='lower left')
 ax.grid()
 
 ax1twin = ax.twinx()
-ax1twin.plot(t, Cms_comp, 'k', linewidth=1, label='Cms compensation')
+ax1twin.plot(t, Cms_comp*1000, 'k', linewidth=1, label='Cms compensation')
+ax1twin.set_ylim([Cms_min*990, Cms_max*1100])
 ax1twin.legend(loc='upper left')
-ax1twin.set(ylabel='Compliance [m/N]')
-
-plt.show()
-
-sys.exit()
-
-#================================================================================
-#================================ Main plot bis =================================
-#================================================================================
-
-fig, ax = plt.subplots(2, 1, sharex = True)
-
-ax[0].plot(t, u, label='Tension | Feed.')
-ax[0].set(xlabel='Time [s]', ylabel='Amplitude [V]', xlim=(tstart, tstart+duration))
-
-ax[0].set_yticks(np.linspace(-A*1.02, A*1.02, 7))
-ax[0].grid()
-
-ax[1].plot(t, x2,'k', label='Displacement | No Feed.')
-ax[1].plot(t, x,label='Displacement | Feed.')
-# ax[1].plot(t, x_peak, 'r--',alpha=0.5, label='x_{peak}')
-ax[1].set(xlabel='Time [s]', ylabel='Displacement [mm]', xlim=(tstart, tstart+duration))
-ax[1].grid()
-ax[1].legend()
-
-ax1twin = ax[1].twinx()
-ax1twin.plot(t, Cms_comp,'r', label='Cms compensation')
-ax1twin.set(ylabel='Compliance [m/N]')
+ax1twin.set(ylabel='Compliance [mm/N]')
 
 plt.show()
