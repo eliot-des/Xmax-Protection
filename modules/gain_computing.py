13f3fee78517732ebd96706cf06db965b2f62de5
Eliot Deschang <eliot.deschang@gmail.com>
2024-10-27 15:24:33 +0100

    Add gain computing funtions.

---
+++ modules/gain_computing.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def limiter(abs_x, thres):
+    return np.minimum(1, thres/abs_x)
+
+def limiter_knee(abs_x, thres, knee):
+    return 1/(1 + (abs_x/thres)**(1/knee))**knee
+
+'''
+def compressor(abs_x, thres, ratio, knee):
+    return np.minimum(1, (thres + (abs_x-thres)/ratio)/abs_x)
+'''
+
+def compressor(abs_x, thres, ratio, knee):
+    return np.minimum(1, (thres*ration + abs_x - thres)/(ratio*abs_x))
+
+
+def compressor_knee(abs_x, thres, ratio, knee):
+    if 2*(abs_x-thres) < -knee:
+        y = abs_x
+    elif 2*abs(abs_x-thres) <= knee:
+        y = abs_x + (1/ratio - 1)*(x-thres + knee/2)**2/(2*knee)
+    else:
+        y = thres + (abs_x-thres)/ratio
+    return y/abs_x
