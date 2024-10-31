d5adf28d434f3ed3b8758d5104b788483c53ec92
spreadflo <florian_marie@ymail.com>
2024-10-31 17:53:59 +0100

    Cleaning & minor changes on feedback v2.

--- Feedback/Feedback_Physical_Approach_v2.py
+++ Feedback/Feedback_Physical_Approach_v2.py
@@ -2,8 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sig 
 from scipy.io.wavfile import read
-from lib import normalize, dynamic_peak_follower2, gain_factor_smoothing_sbs_bis, dynamic_peak_follower_sbs, gain_factor_smoothing_sbs
+import os
 import sys
+fpath = os.path.join(os.getcwd(), 'modules')
+sys.path.append(fpath)
+from dynamic_trackers import dynamic_peak_follower_sbs, gain_factor_smoothing_sbs_bis
+from audio import normalize
 
 plt.rc('lines', linewidth=2)
 plt.rc('font', size=14)
@@ -42,11 +46,11 @@ Fs = 48000
 t = np.arange(0, 5, 1/Fs)
 u = sig.chirp(t, f0=10000, f1=10, t1=5, method='logarithmic')
 
-A = 10               #gain of the amplifier -> Max tension in volts
-u = A*u             #tension in volts
-
-tstart   = 0            #start time in seconds
-duration = 5            #duration time in seconds
+A = 4                   # gain of the amplifier -> Max tension in volts
+u = A*u                 # tension in volts
+ 
+tstart   = 0            # start time in seconds
+duration = 5            # duration time in seconds
 
 u = u[int(tstart*Fs):int((tstart+duration)*Fs)]
 t = t[int(tstart*Fs):int((tstart+duration)*Fs)]
@@ -74,9 +78,6 @@ with open(f'Dataset_T&S/{loudspeaker}.txt', 'r') as f:
         exec(line)
 
 
-#optimal minimal compliance to not 
-R_v1 = (Xmax_*Rec/(A*Bl*Cms))
-
 # Low-frequency approximation of displacement
 B_LF = np.array([0, 0, Bl/Rec])         
 A_LF = np.array([Mms, Rms+Bl**2/Rec, 1/Cms])
@@ -89,22 +90,26 @@ b, a = bilinear2ndOrder(B_LF, A_LF, Fs)
 #================================================================================
 #========== Filter the input voltage to get an estimated displacement ===========
 #================================================================================
-x2 = np.zeros_like(u)        #displacement in m
-x2 = sig.lfilter(b, a, u)    #displacement in m
-x2 = x2*1000                #displacement in mm
+x2 = np.zeros_like(u)        # displacement in m
+x2 = sig.lfilter(b, a, u)    # displacement in m
+x2 = x2*1000                 # displacement in mm
 
 #================================================================================
 #====================== Compensation filter definition ==========================
 #========================= Initializing parameters ==============================
 #================================================================================
 
-R = Xmax_*Rec/(A*Bl*Cms)
+R = Xmax_*Rec/(A*Bl*Cms)            # optimal minimal compliance to respect Xmax. R:=Cms_comp/Cms
 print('Chosen R = ', np.round(R, 3))
 
-Cms_comp = np.zeros(len(u))         #compensation compliance to be adjusted sample by sample
-Cms_comp[0] = Cms
-Cms_min = 0.9*R*Cms                 #minimum compensation compliance is 10% of the real Cms (CHOICE)
+if R <= 1:
+    pass
+else:
+    raise ValueError('Gain is too low or Xmax criteria always satisfied.')
 
+Cms_comp = np.zeros(len(u))         # compensation compliance to be adjusted sample by sample
+Cms_comp[0] = Cms                   # initial compliance value is the one of the speaker
+Cms_min = 0.9*R*Cms                 # minimum compensation compliance with a margin factor
 
 
 B_comp = A_LF
@@ -113,7 +118,7 @@ A_comp = np.array([Mms, Rms+Bl**2/Rec, 1/Cms_comp[0]])
 b_comp, a_comp = bilinear2ndOrder(B_comp, A_comp, Fs)
 
 f = np.geomspace(1, Fs/2, 1000)
-w   = 2*np.pi*f
+w = 2*np.pi*f
 
 #================================================================================
 #================== Main loop - feedback control algorithm ======================
@@ -121,22 +126,18 @@ w   = 2*np.pi*f
 
 #Initializations
 
-wr = 2*np.pi*fs
-Q0 = 1/np.sqrt(2)                               # neutral quality factor
-# Q0 = 1/(Rms+(Bl**2)/Rec)*np.sqrt(Mms/Cms)     # original loudspeaker quality factor
+x        = np.zeros(len(u))    # displacement in mm
+x_lim    = np.zeros(len(u))    # limited displacement in mm
+x_peak   = np.zeros(len(u))    # enveloppe estimator in mm
 
-x        = np.zeros(len(u))    #displacement in mm
-x_lim    = np.zeros(len(u))    #limited displacement in mm
-x_peak   = np.zeros(len(u))    #enveloppe estimator in mm
-
-attack_peak    = 0.00005      # Attack time in seconds
-release_peak   = 0.01         # Release time in seconds
-attack_smooth  = 0.01         # Attack time for the gain smoothing function
-release_smooth = 0.5          # Release time for the gain smoothing function
+attack_peak    = 0.0005        # Attack time in seconds
+release_peak   = 0.001         # Release time in seconds
+attack_smooth  = 0.01          # Attack time for the gain smoothing function
+release_smooth = 0.5           # Release time for the gain smoothing function
 
 
-d_DFI   = np.zeros(4)     # memory buffer for Direct Form I
-d_TDFII = np.zeros(2)   # memory buffer for Transposed Direct Form I
+d_DFI   = np.zeros(4)          # memory buffer for Direct Form I
+d_TDFII = np.zeros(2)          # memory buffer for Transposed Direct Form I
 u_hp    = np.zeros_like(u)
 track_Rms = np.zeros_like(u_hp)
 
@@ -153,26 +154,20 @@ for i in range(1, len(u_hp)):
     x[i] *= 1000
 
     # Peak envelope estimation
-    # x_peak[i] = dynamic_peak_follower_sbs(x[i], x_peak[i-1], attack_peak, release_peak, Fs) # x_peak in mm
-    # Klippel envelope estimation
-    dx = (x[i] - x[i-1]) * Fs
-    x_peak[i] = np.sqrt(x[i]**2 + (dx/wr)**2)
+    x_peak[i] = dynamic_peak_follower_sbs(x[i], x_peak[i-1], attack_peak, release_peak, Fs) # x_peak in mm
 
     #we then compare the peak of the displacement signal with Xmax
     if np.abs(x[i]) > Xmax:
-    #if x_peak[i] > Xmax:
+    # if x_peak[i] > Xmax:
         Cms_target = Cms_min
     else:
         Cms_target = Cms
 
     # we then apply the gain smoothing function to the Cms_comp value of the compensation filter
-    Cms_comp[i] = gain_factor_smoothing_sbs_bis(Cms_target, Cms_comp[i-1], attack_smooth, release_smooth, Fs)
-    Rms_comp = 1/Q0*np.sqrt(Mms/Cms_comp[i]) - (Bl**2/Rec)
-    track_Rms[i] = Rms#Rms_comp
-    #Rms_comp = Rms
+    Cms_comp[i] = gain_factor_smoothing_sbs_bis(Cms_target, Cms_comp[i-1], attack_smooth, release_smooth, Fs)    
 
     #we then compute the new compensation coeff filter (zeros are unchanged)...
-    A_comp = np.array([Mms, Rms_comp+Bl**2/Rec, 1/Cms_comp[i]])
+    A_comp = np.array([Mms, Rms+Bl**2/Rec, 1/Cms_comp[i]])
 
     #compute the digital coefficients
     b_comp, a_comp = bilinear2ndOrder(B_comp, A_comp, Fs)
@@ -191,10 +186,9 @@ for i in range(1, len(u_hp)):
 
 fig, ax = plt.subplots()
 
-ax.plot(t, x, '-.b', linewidth=1, label=f'Estimated displacement (feedback)\n Attack {attack_smooth*1e3:.0f} ms\n Release {release_smooth*1e3:.0f} ms')
-ax.plot(t, x_peak, 'r-', linewidth=1, alpha=0.4, label='x_{peak}')
+ax.plot(t, x, 'b', linewidth=1, label=f'Estimated displacement (feedback)\n Attack {attack_smooth*1e3:.0f} ms\n Release {release_smooth*1e3:.0f} ms')
+# ax.plot(t, x_peak, 'r-', linewidth=1, alpha=0.4, label='x_{peak}')
 ax.plot(t, x2, linewidth=1, alpha=0.2, label='Displacement (no feedback)')
-# ax.plot(t, u_hp, 'm', label='Limited displacement (feedback)')
 ax.axhline(y=Xmax, color='red', linestyle='--', label='+Xmax')
 ax.axhline(y=-Xmax, color='red', linestyle='--', label='-Xmax')
 ax.set_ylabel('Amplitude [mm]')
@@ -204,11 +198,9 @@ ax.grid()
 
 ax1twin = ax.twinx()
 ax1twin.plot(t, Cms_comp*1000, 'k', linewidth=1, label='Cms compensation')
-#ax1twin.plot(t, track_Rms, 'k', linewidth=1, label='Rms compensation')
-# ax1twin.set_ylim([Cms_min*990, Cms_max*1100])
 ax1twin.legend(loc='upper left')
-# ax1twin.set(ylabel='Compliance [mm/N]')
-ax1twin.set(ylabel='Rms comp [kg/s]')
+ax1twin.set(ylabel='Compliance [mm/N]')
 
-# plt.savefig(f'Figures/physical_approach/simulation_{loudspeaker}_settings3.pdf')
+# plt.savefig(f'Figures/physical_approach/simulation_{loudspeaker}.pdf')
 plt.show()
+plt.tight_layout
