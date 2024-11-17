620b70e075a7da6d78fb673dce374826e265958c
spreadflo <florian_marie@ymail.com>
2024-11-17 20:01:25 +0100

    Compensation filter figures for defense1.

--- Physical Approach/speakerCompensation.py
+++ Physical Approach/speakerCompensation.py
@@ -11,7 +11,7 @@ loudspeakers = {'full-range1' :'Dayton_CE4895-8',
                 'woofer3': 'Dayton_RS150-4',
                 'subwoofer1': 'B&C_15FW76-4'}
 
-loudspeaker = loudspeakers['full-range2']
+loudspeaker = loudspeakers['full-range1']
 
 with open(f'Dataset_T&S/{loudspeaker}.txt', 'r') as f:
     lines = f.readlines()
@@ -37,8 +37,8 @@ a = np.array([Mms, Rms+Bl**2/Rec, 1/Cms])
 Xmax = 1.2e-3
 
 w0 = 1/np.sqrt(Mms*Cms)
-# Q0 = 1/(w0*Cms*(Rms+(Bl**2/Rec)))
-Q0 = 1/np.sqrt(2)
+Q0 = 1/(w0*Cms*(Rms+(Bl**2/Rec)))
+# Q0 = 1/np.sqrt(2)
 # A_list = np.geomspace(10, 30, 5)
 A_list = np.linspace(0.1, 1, 7)
 print("Cms ratio C values:", A_list)
@@ -55,12 +55,14 @@ for i in range(len(A_list)):
     
     # Cms_comp = Xmax*Rec/(A_list[i]*Bl)
     Cms_comp = A_list[i]*Cms
-    Rms_comp = 1/Q0 * np.sqrt(Mms/Cms_comp) - (Bl**2/Rec)
+    Rms_comp = Rms
+    # Rms_comp = 1/Q0 * np.sqrt(Mms/Cms_comp) - (Bl**2/Rec)
     R_track[i] = Rms_comp/Rms
 
     # a_comp[i] = [Lec*Mms_comp, Rec*Mms_comp+Lec*Rms_comp, Rec*Rms_comp+Lec/Cms_comp+Bl**2, Rec/Cms_comp]      # with Lec
     a_comp[i] = [Mms_comp, Rms_comp+Bl**2/Rec, 1/Cms_comp]                                                    # without Lec       
 
+R_track[-1] = Rms
 
 #frequency response
 
@@ -82,18 +84,17 @@ plt.rcParams.update({
 plt.rc('lines', linewidth=1.5)
 plt.rc('font', size=11)
 plt.rc('axes', linewidth=0.7, labelsize=16)
-plt.rcParams['axes.facecolor'] = 'none'
+# plt.rcParams['axes.facecolor'] = 'none'
 
 
 fig, ax = plt.subplots(2, 1, sharex=True)
-
-# ax[0].semilogx(f, np.abs(h)*1000, '--r', label='Original')
 
 #create color map for the bands
 colors = plt.cm.viridis(np.linspace(0.1, 0.9, len(A_list)))
 
 for i in range(len(A_list)):
-    ax[0].semilogx(f, np.abs(h*h_comp[i])*1000, color = colors[i], label=f'C = {A_list[i]:.2f} \n R = {R_track[i]:.2f}')   # f'Cms: {A_list[i]:.2e} m/N'
+    ax[0].semilogx(f, np.abs(h*h_comp[i])*1000, color = colors[i], label=f'C = {A_list[i]:.2f}')                            # Rms ratio R NOT displayed
+    # ax[0].semilogx(f, np.abs(h*h_comp[i])*1000, color = colors[i], label=f'C = {A_list[i]:.2f} \n R = {R_track[i]:.2f}')    # Rms ratio R displayed
     ax[1].semilogx(f, np.rad2deg(np.angle(h_comp[i])), color = colors[i])
 
 ax[0].semilogx(f, np.abs(h)*1000, '--r', label='Original')
@@ -102,14 +103,14 @@ ax[0].set(xlim = (20, 210)) #, ylim = (-45, 3)
 ax[0].set_ylabel('Magnitude [mm/V]')
 # ax[0].set_title()
 ax[0].grid(which='both', axis='both')
-ax[0].legend(loc='upper right', ncols=2, frameon=False)
+ax[0].legend(loc='upper right', ncols=2)
 
 ax[1].set(xlabel = 'Frequency [Hz]', ylabel = 'Phase [deg]')
 ax[1].set(xlim = (10, f[-1])) #, ylim = (0, 100)
 ax[1].grid(which='both', axis='both')
 
 plt.tight_layout()
-# fig.savefig('XOuUIn_Conly.pdf', bbox_inches='tight', pad_inches=0)
-fig.savefig('XOuUIn_CandR_test.pdf', format='pdf', bbox_inches='tight', pad_inches=0)
+fig.savefig('XOuUIn_Conly.pdf', format='pdf', transparent=True, bbox_inches='tight', pad_inches=0)
+# fig.savefig('XOuUIn_CandR_test.pdf', format='pdf', transparent=True, bbox_inches='tight', pad_inches=0)
 
 plt.show()
