63c15f2c79c1f75418360bf03c0ab3ce1f5ba93d
Eliot Deschang <eliot.deschang@gmail.com>
2024-12-17 21:33:33 +0100

    Add predicted displacement comparison

--- Measurements/measurement_plugin_process.py
+++ Measurements/measurement_plugin_process.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.signal as sig
 
-
-with np.load('results/shookOnes_wet.npz') as data:
+# ================================================================
+with np.load('Measurements/results/shookOnes_dry.npz') as data:
 
     #measurement parameters
     t = data['t']
-    fs = data['fs']
+    Fs = int(data['fs'])
     G_hp = data['G_hp']
 
     #acquired data
@@ -15,6 +16,20 @@ with np.load('results/shookOnes_wet.npz') as data:
     u_hp = data['u_hp']
     x = data['x']
 
+# ================================================================
+# Load the loudspeaker parameters
+with open(f'Dataset_T&S/Peerless_HDSP830860_Klippel.txt', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        exec(line)
+
+# Low-frequency approximation of X/U(s)
+b_xu = np.array([0, 0, Bl/Rec])         
+a_xu = np.array([Mms, Rms+Bl**2/Rec, 1/Cms])
+bd_xu, ad_xu = sig.bilinear(b_xu, a_xu, fs=Fs)
+
+x_prediction = sig.lfilter(bd_xu, ad_xu, u_hp)
+# ================================================================
 """ PLOT  """
 plt.close("all")
 
@@ -31,9 +46,20 @@ ax[2].plot(t, u_hp, label='Speaker terminal voltage')
 ax[2].set(ylabel=r'$U_{hp}$ [V]')
 ax[2].grid()
 
-ax[3].plot(t-1e-3, 1000*x)  # compensating displacement sensor delay of 1 ms
-#ax[3].plot(t-1e-3, 1000*x_smooth, label='Displacement')
+ax[3].plot(t-0.3e-3, 1e3*x)   #delayed by 0.3 ms -> lazer delay
+ax[3].plot(t, 1e3*x_prediction)
 ax[3].set(xlabel='Time [s]', ylabel='Displacement [mm]')
 ax[3].grid()
 
 plt.show()
+
+'''
+fig, ax = plt.subplots()
+
+r_xxpred = np.correlate(x, x_prediction, mode='full')
+l = np.arange(-len(r_xxpred)//2, len(r_xxpred)//2)
+lmax = l[np.argmax(r_xxpred)]
+print(f"Predicted delay: {lmax/Fs} s")
+ax.plot(l, r_xxpred)
+plt.show()
+'''
