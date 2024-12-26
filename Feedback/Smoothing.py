58c10fee8c776649157b01220b1d106da13896f2
Eliot Deschang <eliot.deschang@gmail.com>
2024-12-26 12:26:52 +0100

    update font and scale of step function

--- Feedback/Smoothing.py
+++ Feedback/Smoothing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rc('lines', linewidth=1.5)
 plt.rcParams.update({'font.size': 19})
 plt.rcParams.update({"text.usetex": True, "font.family": "serif"})
 
@@ -15,7 +16,7 @@ t_end = 0.1
 
 Cms_comp = np.ones(len(t))
 Cms_target = np.ones(len(t))
-Cms_target[int(t_start*Fs):int(t_end*Fs)] = 0
+Cms_target[int(t_start*Fs):int(t_end*Fs)] = 0.5
 
 attack = 0.03
 release = 0.1
@@ -36,7 +37,7 @@ for n in range(1, len(Cms_target)):
     Cms_comp[n] = (1 - k) * Cms_comp[n-1] + k * Cms_target[n]
 
 
-fig, ax = plt.subplots(figsize=(10, 5), layout='constrained')
+fig, ax = plt.subplots(figsize=(10, 4), layout='constrained')
 ax.plot(t*1e3, Cms_target, color='0.2', label=r'$C_{ms}^{(target)}[n]$')
 ax.plot(t*1e3, Cms_comp, color='0.0',label=r'$C_{ms}^{(comp)}[n]$', linestyle='--',dashes=(7, 5))
 
@@ -55,11 +56,11 @@ ax.axvline(t_rise_end*1e3, color='0.5', linestyle=':')
 #add horizontal arrow to between t_fall_start and t_fall_end and for t_rise_start and t_rise_end with 't_a' and 't_r' labels
 #above the middle of the arrows
 
-ax.annotate('', xy=(t_fall_end*1e3, 0.5), xytext=(t_fall_start*1e3, 0.5), arrowprops=dict(arrowstyle='<->'))
-ax.annotate(r'$t_a$', xy=(t_fall_start*1e3 + (t_fall_end*1e3 - t_fall_start*1e3)/2 - 2, 0.53))
+ax.annotate('', xy=(t_fall_end*1e3, 0.75), xytext=(t_fall_start*1e3, 0.75), arrowprops=dict(arrowstyle='<->'))
+ax.annotate(r'$t_a$', xy=(t_fall_start*1e3 + (t_fall_end*1e3 - t_fall_start*1e3)/2 - 2, 3/2 * 0.51))
 
-ax.annotate('', xy=(t_rise_end*1e3, 0.5), xytext=(t_rise_start*1e3, 0.5), arrowprops=dict(arrowstyle='<->'))
-ax.annotate(r'$t_r$', xy=(t_rise_start*1e3 + (t_rise_end*1e3 - t_rise_start*1e3)/2 - 2, 0.53))
+ax.annotate('', xy=(t_rise_end*1e3, 0.75), xytext=(t_rise_start*1e3, 0.75), arrowprops=dict(arrowstyle='<->'))
+ax.annotate(r'$t_r$', xy=(t_rise_start*1e3 + (t_rise_end*1e3 - t_rise_start*1e3)/2 - 2, 3/2*0.51))
 
 ax.set(xlabel='Time [ms]', ylabel=r'$C_{ms}$ [m/N]')
 ax.legend(loc='lower right')
